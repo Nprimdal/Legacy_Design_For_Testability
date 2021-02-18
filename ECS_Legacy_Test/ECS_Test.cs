a52992c9422a22e3c5ddc8d0bad89b67427ef2c3
Nanna Primdal <201811526@post.au.dk>
2021-02-18 13:23:11 +0100

    Test projekt tilføjet

---
+++ ECS_Legacy_Test/ECS_Test.cs
@@ -0,0 +1,32 @@
+using ECS_Legacy_version2;
+using NUnit.Framework;
+
+namespace ECS_Legacy_Test
+{
+    public class ECS_Test
+    {
+        private ITempSensor _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new FakeTempSensor();
+        }
+
+        [TestCase(-5,-5)]
+        [TestCase(0, 0)]
+        [TestCase(2, 2)]
+        [TestCase(10, 10)]
+        [TestCase(17, 17)]
+        [TestCase(24, 24)]
+        [TestCase(35, 35)]
+        [TestCase(45, 45)]
+        public void Test_GetTemp_ExpectedEqualsProperty()
+        {
+
+        }
+
+        
+
+    }
+}
---
+++ ECS_Legacy_Test/FakeTempSensor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECS_Legacy_version2;
+
+namespace ECS_Legacy_Test
+{
+    public class FakeTempSensor : ITempSensor
+    {
+        public int Temp { get; set; }
+        public int GetTemp()
+        {
+            return Temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
