d1f170c09aeb2120579d7ad77482de657709c2a4
Nanna Primdal <201811526@post.au.dk>
2021-02-18 12:53:23 +0100

    New project

---
+++ ECS_Legacy_version2/IHeater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS_Legacy_version2
+{
+    public interface IHeater
+    {
+        void TurnOff();
+        void TurnOn();
+        bool RunSelfTest();
+    }
+}
---
+++ ECS_Legacy_version2/TempSensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ECS_Legacy_version2
+{
+    internal class TempSensor : ITempSensor
+    {
+        private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return gen.Next(-5, 45);
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
